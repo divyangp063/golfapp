7a02bde5c8a63ce50768aa656d2337ff3f35cfbe
Krishna Sai K <ketha.2@buckeyemail.osu.edu>
2024-10-13 16:08:41 -0400

    Added MyCart Component

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import './Home.css'; // Import custom CSS
+import './home.css'; // Import custom CSS
 
 const Home = () => {
     const [order, setOrder] = useState({
         buyQuantity: [0, 0, 0, 0, 0],
         productName: ['Titleist Pro V1', 'Callaway HEX Warbird', 'Callaway HEX Diablo', 'Bridgestone E6', 'Bridgestone B330-RX'],
+        productPrice: [47.99, 14.95, 47.99, 21.95, 37.95],
+       productImage: ['V1XCover.jpg', 'CallawayWarbird.jpg','CallawayDiablo.jpg','BridgestoneE6.jpg','BridgestoneTout.jpg'],
         credit_card_number: '',
         expiry_date: '',
         cvvCode: '',
--- src/components/myCart.js
+++ src/components/myCart.js
@@ -1,13 +1,64 @@
 import React from "react";
 
+import { useNavigate, useLocation } from "react-router-dom";
+
+import './MyCart.css';
+
+
+
+
 const MyCart = () => {
     let title = "My Cart page";
+
+
+
+    const { state } = useLocation();
+    const order = state
+
+    function handleClick(){
+        alert("Redirecting to payments, Total: "+total);
+    }
+
+    const list = []
+    let total = 0;
+
+    for(let i = 0; i<order.buyQuantity.length; i++){
+        if(order.buyQuantity[i]>0){
+
+            list.push(
+    
+                    <div className="item-card">
+                        <div className="image-container">
+                            <img className="item-image" src={require(`../assets/${order.productImage[i]}`)} alt="Item"/>
+                        </div>
+                        <div className="title-container">
+                            <h3>{order.productName[i]}</h3>
+                            <p> quantity: {order.buyQuantity[i]}</p>
+                        </div>
+                        <div className="price-container">
+                            <h3> $ {order.buyQuantity[i] * order.productPrice[i]}</h3>
+                        </div>
+                    </div>
+
+            )
+
+            total = total + order.buyQuantity[i] * order.productPrice[i]
+
+        }
+    }
 
     return (
         <div>
             <h1>
                 {title}
             </h1>
+            <div className="cart-holder">
+                <hr/>
+                    {list}
+            </div>
+
+            <h4>Total: $ {total}</h4>
+            <button onClick={handleClick}> Proceed to Checkout </button>
             <footer className="footer bg-dark text-white mt-5 p-3 text-center">
                 <p>&copy; 2024 Team 3. All Rights Reserved.</p>
             </footer>
