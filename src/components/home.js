e1b98ec32ca36f3225d4b6716e7bda37329dc486
Mohamed Asmali <mohamedasmali@hotmail.com>
2024-11-03 12:42:56 -0500

    change Home -> home

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import './navbar.css';
-import './Home.css'; // Import custom CSS
+import './home.css'; // Import custom CSS
 
 const Home = () => {
     const [order, setOrder] = useState({
