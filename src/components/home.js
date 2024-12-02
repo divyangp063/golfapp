439b35e0edb9529e99ba907bd744098c3504d992
divyangp063 <prajapati.30@buckeyemail.osu.edu>
2024-12-02 00:42:20 -0500

    Just Updating one line

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import './navbar.css';
-import './Home.css'; // Import custom CSS
+import './home.css'; // Import custom CSS
 
 const Home = () => {
     const location = useLocation();
